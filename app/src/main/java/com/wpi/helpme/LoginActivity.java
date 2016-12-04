e73b852d76f6ac275a8b895765c0bb8c1e0bd192
Aditya Nivarthi <adityanivarthi19@gmail.com>
2016-12-04 16:50:24 -0500

    Cleanup and documentation.

--- app/src/main/java/com/wpi/helpme/LoginActivity.java
+++ app/src/main/java/com/wpi/helpme/LoginActivity.java
@@ -1,7 +1,6 @@
 package com.wpi.helpme;
 
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -44,18 +43,24 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // Create Firebase objects
         this.setUpAuthentication();
 
+        // Sign in button
         SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // If user exists, do nothing
+                // If no user, start sign in process
                 FirebaseUser user = mFAuth.getCurrentUser();
                 if (user != null) {
+                    // TODO Remove toast
                     Toast.makeText(getApplicationContext(),
                             "Already signed in as " + user.getDisplayName(), Toast.LENGTH_LONG)
                             .show();
                 } else {
+                    // TODO Remove toast
                     Toast.makeText(getApplicationContext(), "Signing in with Google",
                             Toast.LENGTH_LONG)
                             .show();
@@ -66,22 +71,28 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        // Sign out button
         SignInButton signOutButton = (SignInButton) findViewById(R.id.sign_out_button);
-
         signOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // If no user, do nothing
+                // If user exists, start sign out process
                 if (mFAuth.getCurrentUser() != null) {
+                    // TODO Remove toast
                     Toast.makeText(getApplicationContext(), "Signed out", Toast.LENGTH_LONG)
                             .show();
                     signOutGoogleAccount();
                 } else {
+                    // TODO Remove toast
                     Toast.makeText(getApplicationContext(), "Already signed out", Toast.LENGTH_LONG)
                             .show();
                 }
             }
         });
 
+        // Modify com.google.android.gms.common.SignInButton to say "Sign out"
+        // Referenced from http://stackoverflow.com/questions/18040815/can-i-edit-the-text-of-sign-in-button-on-google
         for (int i = 0; i < signOutButton.getChildCount(); i++) {
             View v = signOutButton.getChildAt(i);
 
@@ -93,25 +104,37 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Sets up the Google sign in authentication objects.
+     *
+     * @see <a href="https://github.com/firebase/quickstart-android/blob/master/auth/app/src/main/java/com/google/firebase/quickstart/auth/GoogleSignInActivity.java">Quickstart
+     * Example</a>
+     */
     protected void setUpAuthentication() {
+        // Google sign in builder
         GoogleSignInOptions gsio = new GoogleSignInOptions.Builder(
                 GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
+        // Google client
         mApiClient = new GoogleApiClient.Builder(this)
                 .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
                     @Override
                     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        // TODO Do something if connection fails
                         Log.d(TAG, "Google API Client connection failed.");
                     }
                 })
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gsio)
                 .build();
 
+        // Get Firebase reference
         firebase = FirebaseDatabase.getInstance().getReference();
         mFAuth = FirebaseAuth.getInstance();
+
+        // Listener for Firebase state changes
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -129,10 +152,13 @@ public class LoginActivity extends AppCompatActivity {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
+        // Check login result
         if (requestCode == RC_LOGIN) {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
 
+            // Successful result
             if (result.isSuccess()) {
+                // Get credentials
                 GoogleSignInAccount acct = result.getSignInAccount();
                 AuthCredential credential = GoogleAuthProvider
                         .getCredential(acct.getIdToken(), null);
@@ -140,9 +166,11 @@ public class LoginActivity extends AppCompatActivity {
                         .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
+                                // Log if successful or not
                                 if (!task.isSuccessful()) {
                                     Log.d(TAG, "signInWithCredential", task.getException());
                                 } else {
+                                    // Get new Firebase instance after logging in
                                     firebase = FirebaseDatabase.getInstance().getReference();
                                     Toast.makeText(getApplicationContext(),
                                             "Firebase database connected", Toast.LENGTH_LONG);
@@ -167,13 +195,19 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Signs the current user out of the application with their Google account.
+     */
     private void signOutGoogleAccount() {
+        // Firebase sign out
+        mFAuth.signOut();
+
+        // Google sign out
         Auth.GoogleSignInApi.signOut(mApiClient).setResultCallback(new ResultCallback<Status>() {
             @Override
             public void onResult(@NonNull Status status) {
                 Log.d(TAG, "Signed out");
                 firebase.onDisconnect();
-                mFAuth.signOut();
             }
         });
     }
