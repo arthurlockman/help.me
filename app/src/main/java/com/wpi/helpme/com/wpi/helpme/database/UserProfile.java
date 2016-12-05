728c165d33bfe62e051b3fc2632578143e697930
Aditya Nivarthi <adityanivarthi19@gmail.com>
2016-12-05 13:40:16 -0500

    Added empty constructor for UserProfile object.

--- app/src/main/java/com/wpi/helpme/com/wpi/helpme/database/UserProfile.java
+++ app/src/main/java/com/wpi/helpme/com/wpi/helpme/database/UserProfile.java
@@ -1,11 +1,18 @@
 package com.wpi.helpme.com.wpi.helpme.database;
 
 public class UserProfile {
+    private String userId;
     private String userName;
     private String email;
 
-    public UserProfile(String email, String userName) {
+    public UserProfile() {
+        this.userId = "";
+        this.email = "";
+        this.userName = "";
+    }
 
+    public UserProfile(String userId, String email, String userName) {
+        this.userId = userId;
         this.email = email;
         this.userName = userName;
     }
@@ -17,5 +24,18 @@ public class UserProfile {
 
     public String getEmail() {
         return email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    @Override
+    public String toString() {
+        return "UserProfile{" +
+                "userId='" + userId + '\'' +
+                ", userName='" + userName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
