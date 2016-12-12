f58a6cd23d62a8b7463dfb65a69fd1fc04cd6d31
Arthur Lockman <hello@rthr.me>
2016-12-11 21:35:58 -0500

    Added push notifications.

---
+++ app/src/main/java/com/wpi/helpme/FirebaseMessagingService.java
@@ -0,0 +1,16 @@
+package com.wpi.helpme;
+
+import com.google.firebase.messaging.RemoteMessage;
+
+public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService
+{
+    public FirebaseMessagingService()
+    {
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage)
+    {
+        super.onMessageReceived(remoteMessage);
+    }
+}
