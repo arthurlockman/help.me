b7ccfe7398b9d5d038d0d901198cc3cee6a1dfdc
Aditya Nivarthi <adityanivarthi19@gmail.com>
2016-12-11 21:47:26 -0500

    Loads profile on app startup if user is still logged in.

--- app/src/main/java/com/wpi/helpme/com/wpi/helpme/profile/UserProfile.java
+++ app/src/main/java/com/wpi/helpme/com/wpi/helpme/profile/UserProfile.java
@@ -71,6 +71,7 @@ public class UserProfile {
                 ", userName='" + userName + '\'' +
                 ", email='" + email + '\'' +
                 ", filters=" + filters +
+                ", deviceToken=" + deviceToken +
                 '}';
     }
 
