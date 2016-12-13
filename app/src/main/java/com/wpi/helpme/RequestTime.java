76875e2baf6db6d873ad3dbf2b2d9c9094ec26e4
Arthur Lockman <hello@rthr.me>
2016-12-12 22:57:34 -0500

    Fixed date/time format

--- app/src/main/java/com/wpi/helpme/RequestTime.java
+++ app/src/main/java/com/wpi/helpme/RequestTime.java
@@ -3,8 +3,13 @@ package com.wpi.helpme;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TimePicker;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static com.wpi.helpme.RequestDescription.NOTES;
 import static com.wpi.helpme.RequestDescription.PHOTO;
@@ -31,13 +36,17 @@ public class RequestTime extends AppCompatActivity {
         timePicker.clearFocus();
         int hour = timePicker.getHour();
         int minute = timePicker.getMinute();
-        String clockTime = hour + ":" + minute;
+        Date d = new Date();
+        d.setHours(hour);
+        d.setMinutes(minute); //Yes I know this is wrong but we needed it to work for demo
+        d.setSeconds(0);
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
         Intent myIntent = new Intent(this, RequestConfirmation.class);
         myIntent.putExtra(TOPIC, bundle.getString(TOPIC));
         myIntent.putExtra(NOTES, bundle.getString(NOTES));
         myIntent.putExtra(TITLE, bundle.getString(TITLE));
-        myIntent.putExtra(TIME, clockTime);
+        myIntent.putExtra(TIME, dateFormat.format(d));
         myIntent.putExtra(PHOTO, getIntent().getStringExtra(PHOTO));
         this.startActivity(myIntent);
     }
